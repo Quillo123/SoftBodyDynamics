54ef24f3abcb8ee101d6ad59d6fbfe27565fd90c
Noah Gallo <gallon1@spu.edu>
2021-07-21 23:04:08 -0700

    Created new Project and added MassPoint Struct

---
+++ SoftBodyObjects/Assets/Scripts/SoftBodyMesh.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SoftBodyMesh
+{
+    Mesh mesh;
+    MeshFilter meshFilter;
+
+    Vector3[] newVertices;
+    Vector2[] newUV;
+    int[] newTriangles;
+
+    public SoftBodyMesh(MeshFilter meshFilter, Mesh mesh = null)
+    {
+        if (mesh)
+        {
+            this.mesh = mesh;
+        }
+        else
+        {
+            mesh = new Mesh();
+        }
+        this.meshFilter = meshFilter;
+        meshFilter.mesh = mesh;
+
+    }
+
+    public Vector3[] GenerateMeshVerticesFromShape(Vector3[] pVertices)
+    {
+        
+
+        return null;
+    }
+}
---
+++ SoftBodyObjects/Assets/Scripts/SoftBodyObject.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class SoftBodyObject : MonoBehaviour
+{
+   
+
+
+
+    public struct MassPoint
+    {
+        const float PRECISION = 0.001f;
+        bool roundForces;
+
+        Vector3 position;
+        Vector3 force;
+        Vector3 velocity;
+        float mass;
+        bool isStatic;
+
+        public MassPoint(Vector3 position, float mass, bool roundForces = true, bool isStatic = false)
+        {
+            this.position = position;
+            this.mass = mass;
+            this.roundForces = roundForces;
+            this.isStatic = isStatic;
+            this.force = Vector3.zero;
+            this.velocity = Vector3.zero;
+        }
+
+        public void AddForce(Vector3 force)
+        {
+            if(Mathf.Abs(force.magnitude) > PRECISION)
+                this.force += force;
+        }
+
+        public void OnUpdate(float deltaTime, bool simulateForces = true)
+        {
+            if(simulateForces && !isStatic)
+            velocity += force * deltaTime;
+            if (Mathf.Abs(velocity.x) < PRECISION)
+                velocity.x = 0;
+            if (Mathf.Abs(velocity.y) < PRECISION)
+                velocity.y = 0;
+            position += velocity * deltaTime;
+        }
+    }
+}
